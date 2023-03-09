405f57e972285cd60d793b38b5ab9207e21567ba
Makarovs <rolandsmakarovs264@gmail.com>
2023-03-09 12:16:36 +0200

    Mesana Metode

--- src/kaulins.java
+++ src/kaulins.java
@@ -1,14 +1,16 @@
 import java.util.Random;
 
 public class kaulins {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-int skaitlis;
+static void mestKaulinu(){
+	int skaitlis;
 Random rand = new Random();
 skaitlis= rand.nextInt(6)+1;
 System.out.println("Uzkrita skaitlis: "+skaitlis);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+mestKaulinu();
 	}
 
 }
