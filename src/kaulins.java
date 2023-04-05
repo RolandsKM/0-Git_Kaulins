1df64be704d664426cb95811a956f861bd1ed9f2
Makarovs <rolandsmakarovs264@gmail.com>
2023-04-05 09:56:24 +0300

    Lielāko skaitļu noteikšana

--- src/kaulins.java
+++ src/kaulins.java
@@ -4,10 +4,16 @@ import java.util.Scanner;
 public class kaulins {
 static void mestKaulinu(int reizes){
 	int skaitlis;
+	int lielakais=0;
 Random rand = new Random();
 for(int i=1; i<=reizes; i++){
 skaitlis= rand.nextInt(6)+1;
 System.out.println("Uzkrita skaitlis: "+skaitlis);
+
+if(lielakais<skaitlis) {
+	lielakais=skaitlis;
+}
+System.out.println("Lielâkais skaitlis no metieniem bija: "+lielakais);
 	}
 }
 	public static void main(String[] args) {
